95cc7571b91b60c0edd8230338e210afdcb44d61
niteshkr034 <81917957+niteshkr034@users.noreply.github.com>
2022-12-23 02:29:19 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+const App = () => {
+  return (
+<div className="app"></div>
+  )
+}
+
+export default App;
