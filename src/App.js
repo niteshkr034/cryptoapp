14d311dafece9738870e4b60ea4d0cf72e563180
niteshkr034 <81917957+niteshkr034@users.noreply.github.com>
2022-12-25 16:09:18 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import {Switch, Route, Link} from 'react-router-dom';
-import { Layout, Typography, Space } from 'antd';
+import React from "react";
+import { Switch, Route, Link, Routes } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+
+import "./App.css";
+import { Colors } from "chart.js";
 
 const App = () => {
   return (
-<div className="app"></div>
-  )
-}
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/cryptocurrencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
